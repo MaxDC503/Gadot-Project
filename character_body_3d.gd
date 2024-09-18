1390b669ec21f696afeec8b9c64582a918d5e859
cutti034 <cutti034@umn.edu>
2024-09-17 20:03:44 -0500

    Functional wall running added

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -17,6 +17,10 @@ const JUMP_MULT_HORIZONTAL = 3.5
 
 # wall run variables
 const MIN_WALL_RUN_VELOCITY = 2.0
+const WALL_RUN_STOP_FORCE = 0.5
+var isWallRunning = false
+var currentWallNormal
+var lastWallNormal
 
 # camera variables
 const MAX_LOOK_ANGLE = 90
@@ -49,7 +53,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y -= GRAVITY * delta
 	
 	# Handle sprint.
-	if Input.is_action_pressed("sprint"):
+	if Input.is_action_pressed("sprint") and not isWallRunning:
 		speed = SPRINT_SPEED
 	else: 
 		speed = WALK_SPEED
@@ -58,19 +62,22 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if is_on_floor():
+	if is_on_floor() and not isWallRunning:
 		if direction:
 			velocity.x = direction.x * speed
 			velocity.z = direction.z * speed
 		else:
 			velocity.x = lerp(velocity.x, direction.x * speed, delta * GROUND_STOP_FORCE)
 			velocity.z = lerp(velocity.z, direction.z * speed, delta * GROUND_STOP_FORCE)
+	elif isWallRunning:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * WALL_RUN_STOP_FORCE)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * WALL_RUN_STOP_FORCE)
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * AIR_STOP_FORCE)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * AIR_STOP_FORCE)
-		
+	
 	# Handle jump.
-	if Input.is_action_pressed("jump"): #and is_on_floor():
+	if Input.is_action_pressed("jump"):
 		if jumpMult <= MAX_JUMP_MULT:
 			jumpMult = lerp(jumpMult, MAX_JUMP_MULT, JUMP_MULT_SCALAR)
 			jumpChargeBar.max_value = MAX_JUMP_MULT
@@ -81,16 +88,37 @@ func _physics_process(delta: float) -> void:
 			velocity.y += JUMP_VELOCITY * jumpMult
 			velocity.x += direction.x * jumpMult * JUMP_MULT_HORIZONTAL
 			velocity.z += direction.z * jumpMult * JUMP_MULT_HORIZONTAL
+		elif isWallRunning:
+			velocity.y += JUMP_VELOCITY * jumpMult
+			velocity.x += direction.x * jumpMult * JUMP_MULT_HORIZONTAL + get_wall_normal().x * 10
+			velocity.z += direction.z * jumpMult * JUMP_MULT_HORIZONTAL + get_wall_normal().z * 10
+			
 		jumpMult = 1.0
 		jumpChargeBar.value = jumpMult
-		
+	
 	# Handle Wallrun.
+	var zVec = Vector3(0, 0, 1)
+	var horizontalSpeed = Vector3(velocity.x, 0, velocity.z).length()
 	
-	if is_on_wall_only():
-		print(get_wall_normal())
-		print("wallrunning")
+	if is_on_floor():
+		lastWallNormal = null 
+		currentWallNormal = null
+	elif not isWallRunning:
+		lastWallNormal = currentWallNormal
 	
-	t_bob += delta * velocity.length() * float(is_on_floor())
+	if is_on_wall_only() and not lastWallNormal == get_wall_normal() and horizontalSpeed > WALK_SPEED + 0.5:
+		isWallRunning = true
+		currentWallNormal = get_wall_normal()
+		if velocity.y > 0.0: 
+			velocity.y = lerp(velocity.y, 0.0, 0.01)
+		else:
+			velocity.y = 0.0
+	else: 
+		isWallRunning = false
+
+
+
+	t_bob += delta * velocity.length() * float(is_on_floor() or isWallRunning)
 	camera.transform.origin = _headbob(t_bob)
 
 	# FOV
